8ba0f2ec7159a91fab8c90a15d173fb4a26e5b4e
Yurii Sychov <yurii@sychov.pp.ua>
2023-01-06 13:06:47 +0200

    2023-01-06_13-06-45

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,10 +1,13 @@
 package controllers;
 
 import interfaces.CollectionAddressBook;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import objects.Person;
 
 public class MainController {
@@ -19,6 +22,9 @@ public class MainController {
     private TableColumn<Person, String> columnPhone;
 
     @FXML
+    private AnchorPane formAddressBook;
+
+    @FXML
     private void initialize() {
         columnFIO.setCellValueFactory(new PropertyValueFactory<Person, String>("fio"));
         columnPhone.setCellValueFactory((new PropertyValueFactory<Person, String>("phone")));
@@ -26,5 +32,33 @@ public class MainController {
         addressBookImpl.fillTestData();
 
         tableAddressBook.setItems(addressBookImpl.getPersonList());
+    }
+
+
+    public void showDialog(ActionEvent actionEvent) {
+        Object source = actionEvent.getSource();
+
+        if (!(source instanceof Button)) {
+            return;
+        }
+
+        Button clickedButton = (Button) source;
+        System.out.println(clickedButton.getId());
+
+        Person seletedPerson = (Person) tableAddressBook.getSelectionModel().getSelectedItem();
+
+
+        switch (clickedButton.getId()) {
+            case "btnAdd":
+                System.out.println("add: " + seletedPerson);
+                formAddressBook.setDisable(false);
+                break;
+            case "btnEdit":
+                System.out.println("edit: " + seletedPerson);
+                break;
+            case "btnDelete":
+                System.out.println("delete: " + seletedPerson);
+                break;
+        }
     }
 }
